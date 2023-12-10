6b970bcd6701a322b1f317a91a56708ff89507ef
Michael White <michaelwhite404@gmail.com>
2023-12-10 15:08:49 -0800

    Add tab navigation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Tab = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}: {navigation: any}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Deployment Test</Text>
+      <Button title="Next Page" onPress={() => navigation.navigate('Next')} />
+    </SafeAreaView>
+  );
+};
+
+const NextPage = ({navigation}: {navigation: any}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>The Next Page</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    </SafeAreaView>
+  );
+};
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Deployment Test</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Next" component={NextPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -21,6 +39,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
   },
 });
 
